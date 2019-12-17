fb68eaff55a0c4e49091d9b5f8ac4b0ba4861c6f
Andy Kay <andykay.me@gmail.com>
2019-12-17 14:43:57 -0500

    add chakra-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
 import React from "react"
+import { Box, Heading, Text, Link } from "@chakra-ui/core"
 
 export default () => (
-  <div>
-    <h2>👋 Hi, I'm Andy Kay.</h2>
-  </div>
+  <Box p={6}>
+    <Heading>
+      <span role="img" aria-label="waving hand emoji">
+        👋
+      </span>{" "}
+      Hi! I'm Andy Kay.
+    </Heading>
+
+    <Text>
+      I build web applications. I write about how I build them under /blog
+    </Text>
+
+    <Text>
+      Chat with me here{" "}
+      <Link href="http://bit.ly/38QeFX1" isExternal>
+        http://bit.ly/38QeFX1
+      </Link>
+    </Text>
+  </Box>
 )
