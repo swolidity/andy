0449366d85168d1bd42c49fdaad4c90135b6a0b7
Andy Kay <andykay.me@gmail.com>
2020-01-07 11:10:40 -0500

    added mdx layout component and started writing building lists post

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-mdx`, `gatsby-plugin-postcss`],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+    `gatsby-plugin-postcss`,
+  ],
 }
---
+++ src/components/layout.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default ({ children }) => (
+  <div class="markdown p-6 max-w-xl mx-auto my-0">{children}</div>
+)
